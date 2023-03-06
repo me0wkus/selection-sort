197d274d1a7c24abf036c12d45abf9ce8705a72e
me0wkus <118027059+me0wkus@users.noreply.github.com>
2023-03-07 01:34:01 +0300

    Add files via upload

---
+++ Selection Sort.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    const int N = 10;
+    int array[N] = { 3, 5, 7, 4, 9, 2, 6, 1, 8, 10 };
+    int min;
+    int i;
+    
+
+        /*сортировка*/
+        for (int i = 0; i < N - 1; i++)
+        {
+            min = i; 
+            for (int j = i + 1; j < N; j++)
+            {
+                if (array[j] < array[min])
+                {
+                    min = j;
+                }
+            }
+            //перебираем
+            int buf = array[i];
+            array[i] = array[min];
+            array[min] = buf;
+
+        }
+        for (i = 0; i < N; i++) {
+            cout << array[i] << '\t';
+            cout << endl; 
+        }
+    return 0;
+}
